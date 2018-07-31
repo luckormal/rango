ed44e1d11b73530342a582cc77a1d4aac30a3cef
Lucjan K.M <lkm@o2.pl>
2018-07-31 22:27:32 +0200

    Added index.html template, 4.1

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -29,6 +28,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
