5a0933f7cd544048c208761c1cbcf1a068508e61
Lucek_work <lkm@o2.pl>
2018-08-17 13:15:45 +0200

    Chapter 5 cleared, test almost

--- populate_rango.py
+++ populate_rango.py
@@ -43,13 +43,13 @@ def populate():
         {"title":"Bociany",
          "url":"http://www.bocianyonline.pl/kamery.html"}]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages},
-            "Polska": {"pages": polish_pages} }
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16},
+            "Polska": {"pages": polish_pages, "views": 16, "likes": 8} }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -64,8 +64,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
     c.save()
     return c
 
