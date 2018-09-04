ca7bb07d3f9339a84b6f17afde59df7cd294aa0e
Lucjan K.M <lkm@o2.pl>
2018-09-04 23:38:06 +0200

    working on test, something with self.assertContains doesn't work

--- rango/models.py
+++ rango/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    views = models.IntegerField(default=0)
+    views = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(default="test")
+
+    def save(self, *args, **kwargs):
+        if self.views < 0:
+            self.views = 0
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
