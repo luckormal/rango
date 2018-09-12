70a280d22d7aacf3b18efae6618235d0eb025bf3
Lucek_work <lkm@o2.pl>
2018-09-12 14:14:55 +0200

    Added gitignore and working with forms. Start at GET and POST

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
@@ -68,3 +69,30 @@ def luc(request):
     {'name': 'Tokyo', 'population': '33,000,000', 'country': 'Japan'},
     ]}
     return render(request, 'rango/luc.html', context=context_cities )
+
+def add_category(request):
+    form = CategoryForm()
+
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the nwe category to the database.
+            form.save(commit=True)
+            # Now that the category is saved
+            # We could give a confirmation message
+            # But since the most recent category added is on the index page
+            # Then we can direct the user back to the index page.
+            return index(request)
+        else:
+            # The supplied form contained errors -
+            # just print them to the terminal.
+            print(form.errors)
+
+    # Will handle the bad form, new form, or no form supplied cases.
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
